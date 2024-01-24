d930ad8800e05f43b0e6e89d56b09bb16141a72f
sh4m3 <45822792+sh4m3-thecoder@users.noreply.github.com>
2024-01-24 18:22:21 +0700

    readded because it fucked up my config

---
+++ .config/nvim/lua/config/coding.lua
@@ -0,0 +1,306 @@
+
+		--Navic setup
+		require("mason").setup()
+		require("mason-nvim-dap").setup()
+		require("mason-lspconfig").setup()
+
+		local navic = require("nvim-navic")
+		navic.setup({
+			icons = {
+				File = " ",
+				Module = " ",
+				Namespace = " ",
+				Package = " ",
+				Class = " ",
+				Method = " ",
+				Property = " ",
+				Field = " ",
+				Constructor = " ",
+				Enum = " ",
+				Interface = " ",
+				Function = " ",
+				Variable = " ",
+				Constant = " ",
+				String = " ",
+				Number = " ",
+				Boolean = " ",
+				Array = " ",
+				Object = " ",
+				Key = " ",
+				Null = " ",
+				EnumMember = " ",
+				Struct = " ",
+				Event = " ",
+				Operator = " ",
+				TypeParameter = " ",
+			},
+			highlight = true,
+			separator = " > ",
+			depth_limit = 0,
+			depth_limit_indicator = "...",
+			safe_output = true,
+		})
+
+		vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+
+		-- Mappings.
+		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "<space>q", "<cmd>TroubleToggle<cr>", opts)
+
+		-- Use an on_attach function to only map the following keys
+		-- after the language server attaches to the current buffer
+		local on_attach = function(client, bufnr)
+			-- Enable completion triggered by <c-x><c-o>
+			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+			-- Mappings.
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, bufopts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, bufopts)
+			vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+			vim.keymap.set("n", "<space>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, bufopts)
+			vim.api.nvim_create_autocmd("CursorHold", {
+				buffer = bufnr,
+				callback = function()
+					---@diagnostic disable-next-line: redefined-local
+					local opts = {
+						focusable = false,
+						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+						border = "rounded",
+						source = "always",
+						prefix = " ",
+						scope = "cursor",
+					}
+					vim.diagnostic.open_float(nil, opts)
+				end,
+			})
+
+			--if client == "ccls" then
+			--	client.server_capabilities.documentFormattingProvider = false
+			--	client.server_capabilities.documentRangeFormattingProvider = false
+			--end
+
+			if client.server_capabilities.documentSymbolProvider then
+				navic.attach(client, bufnr)
+			end
+		end
+
+		--diagnostics
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+
+		-- Setup lspconfig.
+		--
+		local lspconfig = require("lspconfig")
+
+		local servers = { "ccls", "lua_ls", "pyright", "marksman", "rust_analyzer", "tsserver", "denols", "texlab" }
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		for _, lsp in pairs(servers) do
+			lspconfig[lsp].setup({
+				on_attach = on_attach,
+				flags = {
+					debounce_text_changes = 150,
+					capabilities = capabilities,
+				},
+			})
+		end
+
+		lspconfig["ccls"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			init_options = {
+				highlight = {
+					isRanges = true,
+				},
+				compilationDatabaseDirectory = "build",
+				index = {
+					threads = 12,
+				},
+				clang = {
+					excludeArgs = { "-frounding-math" },
+				},
+				client = {
+					snippetSupport = true,
+				},
+			},
+		})
+
+		lspconfig["lua_ls"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						global = { "vim" },
+						disable = { "undefined-global" },
+					},
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = { enable = false },
+			},
+		})
+
+		lspconfig["tsserver"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			root_dir = lspconfig.util.root_pattern("package.json"),
+		})
+
+		lspconfig["denols"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+		})
+
+		lspconfig["texlab"].setup({
+			cmd = { "texlab" },
+			filetypes = { "tex", "bib" },
+			settings = {
+				texlab = {
+					rootDirectory = nil,
+					forwardSearch = {
+						executable = "evince",
+						args = { "%p" },
+					},
+				},
+			},
+		})
+
+		local has_words_before = function()
+			local unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
+		require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_snipmate").lazy_load()
+		local lspkind = require("lspkind")
+		local luasnip = require("luasnip")
+		local cmp = require("cmp")
+
+		cmp.setup({
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol",
+					max_width = 60,
+					ellipsis_char = "...",
+					preset = "codicons",
+					before = function(entry, vim_item)
+						vim_item.menu = ({
+							nvim_lsp = "[LSP]",
+							look = "[Dict]",
+							luasnip = "[Snippet]",
+							buffer = "[Buffer]",
+						})[entry.source.name]
+						return vim_item
+					end,
+				}),
+			},
+
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					-- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For vsnip users.
+				-- { name = 'ultisnips' }, -- For ultisnips users.
+				-- { name = 'snippy' }, -- For snippy users.
+				{ name = "buffer" },
+			}),
+		})
+
+		-- Set configuration for specific filetype.
+		cmp.setup.filetype("gitcommit", {
+			sources = cmp.config.sources({
+				{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+
+		-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		--Treesitter Config
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "python", "rust", "cpp", "latex", "lua", "javascript", "typescript" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
