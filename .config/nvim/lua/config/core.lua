ecc20bf78347d742ed94736e35750bd53bab9f43
sh4m3 <45822792+sh4m3-thecoder@users.noreply.github.com>
2024-02-18 22:30:21 +0700

    reformat code

--- .config/nvim/lua/config/core.lua
+++ .config/nvim/lua/config/core.lua
@@ -10,24 +10,11 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.autoindent = true
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamedplus"
 
 --Global Customization
 vim.opt.lazyredraw = true
+vim.opt.termguicolors = true
 
 -- Shell Customization
 vim.opt.shell = "zsh"
-
---nvim notify
-local status, notify = pcall(require, "notify")
-if not status then
-	print("Nvim Notify is not installed")
-else
-	vim.notify = require("notify")
-end
-
-notify.setup({
-	fps = 60,
-	stages = "slide",
-	timeout = 2500,
-})
