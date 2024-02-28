96950aa7b72ea2d99dc54e30e3f0c95f7929c137
sh4m3 <45822792+sh4m3-thecoder@users.noreply.github.com>
2024-02-28 20:19:22 +0700

    changed dashboard to alpha

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -8,7 +8,7 @@ return {
 				theme = "auto",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
-				disabled_filetypes = { "NvimTree", "dashboard", "TelescopePrompt", "dashboardpreview", "toggleterm" },
+				disabled_filetypes = { "NvimTree", "alpha", "TelescopePrompt", "dashboardpreview", "toggleterm" },
 				always_divide_middle = true,
 				globalstatus = true,
 			},
