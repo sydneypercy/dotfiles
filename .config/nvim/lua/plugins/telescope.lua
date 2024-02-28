ed82ab08a041de2d3c9e7b988eba7088c9a2db7d
sh4m3 <45822792+sh4m3-thecoder@users.noreply.github.com>
2024-02-28 20:19:35 +0700

    added native fzf for better searching

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,24 +7,28 @@ return {
 			local tsc = require("telescope")
 
 			tsc.setup({
-				defaults= {
+				defaults = {
 					sorting_strategy = "ascending",
 					layout_strategy = "horizontal",
 					layout_config = {
 						horizontal = {
 							prompt_position = "top",
-						}
-					}
+						},
+					},
 				},
 				extensions = {
 					file_browser = {
 						theme = "ivy",
 						hijack_netrw = true,
 					},
+					fzf = {
+						fuzzy = true,
+					},
 				},
 			})
 
 			tsc.load_extension("file_browser")
+			tsc.load_extension("fzf")
 		end,
 	},
 }
