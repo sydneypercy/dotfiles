6bc0de5fad7285cc0f94eb92a24e52800c0c5d0b
sh4m3-thecoder <45822792+sh4m3-thecoder@users.noreply.github.com>
2024-11-15 14:35:35 +0700

    finally figure out how to get 4 spaces to format

--- config/nvim/lua/plugins/guard.lua
+++ config/nvim/lua/plugins/guard.lua
@@ -8,10 +8,17 @@ local thing = {
 		ft("lua"):fmt("lsp"):append("stylua")
 		ft("cpp"):fmt("clang-format")
 
-		require("guard").setup({
+		ft("typescript,javascript,html,css,scss,json,jsonc"):fmt({
+			cmd = "npx",
+			args = { "prettier", "--tab-width", "4", "--trailing-comma", "none", "--stdin-filepath" },
+			fname = true,
+			stdin = true,
+		})
+
+		vim.g.guard_config = {
 			fmt_on_save = true,
 			lsp_as_default_formatter = false,
-		})
+		}
 	end,
 }
 
