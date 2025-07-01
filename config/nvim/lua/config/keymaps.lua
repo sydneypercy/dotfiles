8c2b58aecdc0a571dde9fd3065e80bebb374a9bd
sh4m3-thecoder <45822792+sh4m3-thecoder@users.noreply.github.com>
2025-07-01 13:02:54 +0700

    remove telescope keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,6 @@ INSERT = "i"
 VISUAL = "v"
 COMMAND = "c"
 
-
 local function keymaps(tbl)
 	for i, val in pairs(tbl) do
 		local mode = {}
@@ -34,28 +33,15 @@ vim.keymap.set("n", "<C-Tab>", "<cmd>BufferLineCycleNext<CR>", ns)
 vim.keymap.set("n", "<S-C-Tab>", "<cmd>BufferLineCyclePrev<CR>", ns)
 
 keymaps({
-	["<leader>fwb"] = { mode = { NORMAL }, "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find word in buffer" } },
-	["<leader>fwp"] = { mode = { NORMAL }, "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy word in current working directory" } },
-	["<leader>ff"] = { mode = { NORMAL }, "<cmd> Telescope fd<cr>", { desc = "Find files" } },
-	["<leader>fr"] = { mode = { NORMAL }, "<cmd> Telescope oldfiles<cr>", { desc = "Find recent files" } },
 	["<leader>fn"] = { mode = { NORMAL }, "<cmd>enew<cr>", { desc = "New File" } },
-	["<leader>fh"] = { mode = { NORMAL }, "<cmd>Telescope help_tags<cr>", { desc = "Search help tags" } },
-	["<leader>fb"] = { mode = { NORMAL }, "<cmd>Telescope buffers<cr>", { desc = "Find buffer" } },
-	["<leader>bf"] = { mode = { NORMAL }, "<cmd>Telescope file_browser<cr>", { desc = "Find buffer" } },
 	["<leader>bp"] = { mode = { NORMAL }, "<cmd>BufferLinePick<cr>", { desc = "Pick a buffer to choose" } },
 	["<leader>bd"] = { mode = { NORMAL }, "<cmd>BufferLinePickClose<cr>", { desc = "Pick a buffer to close" } },
 	["<leader>E"] = { mode = { NORMAL }, "<cmd>NvimTreeToggle<cr>", { desc = "Open explorer" } },
-	["<leader>CRp"] = { mode = { NORMAL }, "<cmd>CompetiTest receive problem<cr>", { desc = "Recieve problem" } },
-	["<leader>CRc"] = { mode = { NORMAL }, "<cmd>CompetiTest receive contest<cr>", { desc = "Recieve contest" } },
-	["<leader>CRt"] = { mode = { NORMAL }, "<cmd>CompetiTest receive testcases<cr>", { desc = "Recieve testcases" } },
-	["<leader>Cr"] = { mode = { NORMAL }, "<cmd>CompetiTest run<cr>", { desc = "Run testcases" } },
-	["<leader>Ca"] = { mode = { NORMAL }, "<cmd>CompetiTest add_testcase<cr>", { desc = "Add Testcases" } },
-	["<leader>th"] = { mode = { NORMAL }, "<cmd>Telescope colorscheme<cr>", { desc = "Change colorscheme" } },
 	["<leader>db"] = { mode = { NORMAL }, "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle Breakpoint" } },
 	["<leader>dt"] = { mode = { NORMAL }, "<cmd>DapTerminate<cr>", { desc = "Terminate Debuggin Session" } },
 	["<leader>dc"] = { mode = { NORMAL }, "<cmd>DapContinue<cr>", { desc = "Start Debugging / Continue Breakpoint" } },
 	["<C-d>"] = { mode = { NORMAL }, "<C-d>zz" },
 	["<C-u>"] = { mode = { NORMAL }, "<C-u>zz" },
 	["n"] = { mode = { NORMAL }, "nzzzv" },
-	["N"] = { mode = { NORMAL }, "Nzzzv" }
+	["N"] = { mode = { NORMAL }, "Nzzzv" },
 })
