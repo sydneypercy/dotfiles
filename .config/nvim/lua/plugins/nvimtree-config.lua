8d8410c1fc7dc59e4ecc7ccf33ec670d30e3e9c3
Sydney Percy <sydneyabby@cumallover.me>
2025-09-19 21:44:12 +0700

    changed nvimtree-config

--- .config/nvim/lua/plugins/nvimtree-config.lua
+++ .config/nvim/lua/plugins/nvimtree-config.lua
@@ -6,11 +6,6 @@ return {
 			sort_by = "case_sensitive",
 			view = {
 				adaptive_size = true,
-				mappings = {
-					list = {
-						{ key = "u", action = "dir_up" },
-					},
-				},
 			},
 			renderer = {
 				icons = {
