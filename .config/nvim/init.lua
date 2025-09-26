1e626b0330df2531ad544f792dc7091e45506569
Sydney Percy <sydneyabby@cumallover.me>
2025-09-26 10:10:21 +0700

    changed init.lua and lazylock

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,5 @@
 require("config")
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-
---Bare Essentials
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-})
 --Special to neovide
 if vim.g.neovide then
 	vim.o.guifont = "Cascadia Code NF:h15"
